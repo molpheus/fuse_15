c13a6008c423e6797632b1d8f6a8c9fbc399423a
rouge00al <voiceless.guignol@gmail.com>
2014-05-17 12:20:36 +0900

    TitleScene,ClearScene,GameoverSceneの追加

---
+++ project/Assets/GameOver/GameOverScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			//Enterを押したらシーン切り替え.
+			Application.LoadLevel(0);
+		}
+	}
+}
---
+++ project/Assets/TitleScene/TitleScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.Space))
+		//if(Input.GetButtonDown("Jump"))
+		{
+			//spaceを押したらシーン切り替え.
+			Application.LoadLevel(1);
+		}
+	}
+
+	void OnGUI()
+	{
+		//ディスプレイの解像度を800×600と仮定する.
+		GUI.matrix = Matrix4x4.TRS ( Vector3.zero, Quaternion.identity,
+		                            new Vector3( Screen.width / WIDTH, Screen.height / HEIGHT, 1));
+	}
+}
